b4e643eb4f70de25f78527c6352532cbf3231622
Imran Hussain <imranhussain21309@gmail.com>
2025-06-21 19:53:53 +0530

    Change front end url

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const bodyParser = require("body-parser");
 const cors = require("cors")
 require("dotenv").config();
 const app = express();
+const allowedOrigins = ['https://task-management-stytem-07.netlify.app'];
 
-app.use(cors());
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
